df9319d77eb8a7923933f38a7a332a59a38d6679
lethanhpa <lethanhpa1406@gmail.com>
2023-05-26 08:48:26 +0700

    commit part 13

--- backend-nodejs/constants/jwtSettings.js
+++ backend-nodejs/constants/jwtSettings.js
@@ -1,5 +1,5 @@
 module.exports = {
-    SECRET: 'ADB57C459465E3ED43C6C6231E3C9',
-    AUDIENCE: 'aptech.io',
-    ISSUER: 'softech.cloud',
-  };
+  SECRET: 'ADB57C459465E3ED43C6C6231E3C9',
+  AUDIENCE: 'SE397N.io',
+  ISSUER: 'softech.cloud',
+};
